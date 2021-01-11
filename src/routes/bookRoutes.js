4088c5adfc57bb89c9228808debf2d7aa63c952d
its-Saab <musaababs@gmail.com>
2021-01-11 18:31:59 +0100

    Books library using SQL

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,44 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes');
+
+const bookRouter = express.Router();
+
+function router(nav) {
+  bookRouter.route('/')
+    .get((req, res) => {
+      (async function query() {
+        const request = new sql.Request();
+        const { recordset } = await request.query('select * from books');
+        res.render('bookListView', {
+          nav,
+          title: 'Library',
+          books: recordset,
+        });
+      }());
+    });
+
+  bookRouter.route('/:id')
+    .all((req, res, next) => {
+      (async function query() {
+        const { id } = req.params;
+        const request = new sql.Request();
+        const { recordset } = await request.input('id', sql.Int, id)
+          .query('select * from books where id = @id');
+        [req.book] = recordset;
+        next();
+      }());
+    })
+    .get((req, res) => {
+      res.render('bookView',
+        {
+          nav,
+          title: 'Library',
+          book: req.book
+        });
+    });
+  return bookRouter;
+}
+
+module.exports = router;
